e71eb5bdb631c05d820b3af9ffe581c81b0fff04
Algo Tech <tairocruise@yahoo.com>
2023-06-13 20:07:34 +0300

    one

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+const appName = "Algo-e Tech";
+$(document).ready(function () {
+    parseAndRender();
+    $('title[id="pageTitle"]').html(appName);
+    $('body').removeClass('d-none').addClass('d-block');
+
+    // stick nav
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var navbar = $(".navbar");
+
+        if (scroll > 50) {
+            navbar.addClass("sticky");
+        } else {
+            navbar.removeClass("sticky");
+        }
+    });
+
+});
+function parseAndRender() {
+    var elements = $("*");
+    elements.each(function () {
+        var attributes = this.attributes;
+        for (var i = 0; i < attributes.length; i++) {
+            var attributeName = attributes[i].name;
+            var attributeValue = attributes[i].value;
+            if (/\{{2}\s*print\('([^']+)'\)\s*\}{2}/.test(attributeValue)) {
+                var renderedValue = parseAttributeValue(attributeValue);
+                $(this).attr(attributeName, renderedValue);
+            }
+        }
+    });
+}
+
+function parseAttributeValue(attributeValue) {
+    var renderedValue = attributeValue.replace(/\{{2}\s*print\('([^']+)'\)\s*\}{2}/g, function (match, p1) {
+        return p1;
+    });
+
+    return renderedValue;
+}
